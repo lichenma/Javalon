1e5bbdf15344914a5f14c68c00a09b33d15b4200
Lichen Ma <lichenma123@gmail.com>
2019-04-28 20:14:57 -0400

    update

--- Javalon/src/main/webapp/js/main.js
+++ Javalon/src/main/webapp/js/main.js
@@ -13,6 +13,9 @@ var modal = document.getElementById('myModal');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
   modal.style.display = "none";

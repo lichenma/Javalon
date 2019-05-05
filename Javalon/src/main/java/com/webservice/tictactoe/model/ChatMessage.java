c876840b170767f7fae6df9bcf1fbc2635ef6273
Lichen Ma <lichenma123@gmail.com>
2019-05-05 13:27:53 -0400

    update

--- Javalon/src/main/java/com/webservice/tictactoe/model/ChatMessage.java
+++ Javalon/src/main/java/com/webservice/tictactoe/model/ChatMessage.java
@@ -15,7 +15,8 @@ public class ChatMessage {
         MOVE,
         PROPOSE_TEAM,
         VOTE_TEAM,
-        GAME_INFO
+        REJECT_TEAM, 
+        APPROVE_TEAM
     }
 
     public MessageType getType() {
